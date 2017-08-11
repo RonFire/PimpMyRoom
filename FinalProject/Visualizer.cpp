48bc54da11d5e2b4cb658843aa9879082b1b7635
ronaldbrenner <ronaldbrenner@web.de>
2017-08-11 20:00:44 +0200

    Visualizer class added

---
+++ FinalProject/Visualizer.cpp
@@ -0,0 +1,9 @@
+//
+//  Visualizer.cpp
+//  FinalProject
+//
+//  Created by Ronald Brenner on 11.08.17.
+//  Copyright © 2017 rapidRonald. All rights reserved.
+//
+
+#include "Visualizer.hpp"
---
+++ FinalProject/Visualizer.hpp
@@ -0,0 +1,14 @@
+//
+//  Visualizer.hpp
+//  FinalProject
+//
+//  Created by Ronald Brenner on 11.08.17.
+//  Copyright © 2017 rapidRonald. All rights reserved.
+//
+
+#ifndef Visualizer_hpp
+#define Visualizer_hpp
+
+#include <stdio.h>
+
+#endif /* Visualizer_hpp */
