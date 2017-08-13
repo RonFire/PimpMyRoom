2dff51aa41425c1ef8b2b9f1bc75840da41f3f06
ronaldbrenner <ronaldbrenner@web.de>
2017-08-13 21:22:48 +0200

    Texturen hinzugefügt

--- FinalProject/Visualizer.cpp
+++ FinalProject/Visualizer.cpp
@@ -108,6 +108,15 @@ int Visualizer::doVisualisation() {
 	
 	Shader ourShader("VertexShader.glsl", "FragmentShader.glsl");
 	
+	// ===============
+	// Texture Code
+	// ===============
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
 	//	=======================
 	//	rendering loop starts here
 	//	=======================
