4a18bd551dd219240ec3c2f40c55ad015aa2b495
ronaldbrenner <ronaldbrenner@web.de>
2017-08-19 14:08:39 +0200

    files added

--- FinalProject/shaders/FragmentShader.glsl
+++ FinalProject/shaders/FragmentShader.glsl
@@ -2,11 +2,35 @@
 out vec4 FragColor;
 
 in vec2 texCoord;
+in vec3 Normal;
+in vec3 FragPosition;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+//uniform sampler2D texture1;
+//uniform sampler2D texture2;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPosition;
+uniform vec3 viewPosition;
 
 void main()
 {
-	FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
+	float ambientStrength = 0.1;
+	vec3 ambient = ambientStrength * lightColor;
+	
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPosition - FragPosition);
+	
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColor;
+	
+	float specularStrength = 0.5;
+	vec3 viewDir = normalize(viewPosition - FragPosition);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+	vec3 specular = specularStrength * spec * lightColor;
+	
+	vec3 result = (ambient + diffuse + specular) * objectColor;
+	FragColor = vec4(result, 1.0);
+//	FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
 }
